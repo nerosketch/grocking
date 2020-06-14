67f25db68bbca185c142f706037dab55a8429b93
Dmitry Novikov <nerosketch@gmail.com>
2020-06-14 21:20:33 +0300

    initial

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: ns
+ *
+ * Created on June 14, 2020, 8:39 PM
+ */
+
+#include <cstdlib>
+
+#include "search_algs.h"
+
+using namespace std;
+
+
+
+int main (int argc, char** argv)
+{
+    test_bin_search();
+    return 0;
+}
---
+++ search_algs.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   search_algs.h
+ * Author: ns
+ *
+ * Created on June 14, 2020, 9:04 PM
+ */
+
+#ifndef SEARCH_ALGS_H
+#define SEARCH_ALGS_H
+
+
+/*
+ * Бинарный поиск
+ */
+int test_bin_search ();
+
+
+#endif /* SEARCH_ALGS_H */
+
